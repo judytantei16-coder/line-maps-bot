41d4e6e07d56c048d4bd9e3bae90a4d2af3a95aa
judytantei16-coder <judytantei16@gmail.com>
2026-08-18 22:19:14 +0900

    Update typeLabels.js

--- typeLabels.js
+++ typeLabels.js
@@ -2,29 +2,94 @@
 // 上から順にマッチさせるので、より具体的な分類を上に書く
 // 参考: https://developers.google.com/maps/documentation/places/web-service/place-types
 
-const TYPE_LABELS = {
+// 判定の優先順位（上にあるものほど「その場所の本来の業種」として優先される）
+// atm・bank・parkingなどは他業態に併設されることが多いため、優先順位を下げてある
+const PRIORITY_ORDER = [
+  // 小売（コンビニ等の主業態を、ATM等の付帯設備より優先）
+  "convenience_store",
+  "supermarket",
+  "department_store",
+  "shopping_mall",
+  "clothing_store",
+  "hardware_store",
+  "home_goods_store",
+  "electronics_store",
+
+  // 飲食
+  "restaurant",
+  "cafe",
+  "bakery",
+  "bar",
+  "meal_takeaway",
+
   // 美容・健康
+  "hair_care",
+  "beauty_salon",
+  "spa",
+  "gym",
+
+  // 医療
+  "hospital",
+  "doctor",
+  "dentist",
+  "pharmacy",
+  "physiotherapist",
+
+  // 建設・現場関連
+  "general_contractor",
+  "electrician",
+  "plumber",
+  "roofing_contractor",
+  "storage",
+
+  // 宿泊・不動産
+  "lodging",
+  "real_estate_agency",
+
+  // 公共・教育・宗教
+  "city_hall",
+  "post_office",
+  "police",
+  "fire_station",
+  "school",
+  "university",
+  "place_of_worship",
+  "park",
+  "library",
+
+  // 交通（駅・バス停は単独施設なので中間の優先度）
+  "train_station",
+  "bus_station",
+  "gas_station",
+  "car_repair",
+  "car_wash",
+
+  // 付帯設備・汎用施設（他業態に併設されやすいので最後）
+  "bank",
+  "atm",
+  "parking",
+];
+
+// 日本語ラベルの辞書
+const TYPE_LABELS = {
   hair_care: "美容院",
   beauty_salon: "美容院",
   spa: "エステ・スパ",
   gym: "ジム",
-
-  // 駐車・交通
   parking: "駐車場",
   gas_station: "ガソリンスタンド",
   car_repair: "自動車整備工場",
   car_wash: "洗車場",
   train_station: "駅",
   bus_station: "バス停",
-
-  // 飲食
-  restaurant: "レストラン",
-  cafe: "カフェ",
+  restaurant: "飲食店",
+  cafe: "飲食店",
   bakery: "パン屋",
-  bar: "バー",
-  meal_takeaway: "テイクアウト店",
-
-  // 小売
+  bar: "飲食店",
+  meal_takeaway: "飲食店",
+  meal_delivery: "飲食店",
+  food: "飲食店",
+  night_club: "飲食店",
   convenience_store: "コンビニエンスストア",
   supermarket: "スーパーマーケット",
   department_store: "百貨店",
@@ -33,8 +98,6 @@ const TYPE_LABELS = {
   hardware_store: "ホームセンター",
   home_goods_store: "生活雑貨店",
   electronics_store: "家電量販店",
-
-  // 医療・公共
   hospital: "病院",
   doctor: "医院",
   dentist: "歯科医院",
@@ -46,37 +109,71 @@ const TYPE_LABELS = {
   atm: "ATM",
   police: "警察署",
   fire_station: "消防署",
-
-  // 宿泊・不動産
   lodging: "宿泊施設",
   real_estate_agency: "不動産会社",
-
-  // 建設・現場関連（現場用途で頻出しそうなもの）
   general_contractor: "建設会社",
   electrician: "電気工事業者",
   plumber: "配管工事業者",
   roofing_contractor: "屋根工事業者",
   storage: "倉庫・トランクルーム",
-
-  // 教育・宗教・その他施設
   school: "学校",
   university: "大学",
   place_of_worship: "宗教施設",
   park: "公園",
   library: "図書館",
-  gym_school: "体育館",
 };
+
+// 建物名に含まれやすいキーワードから「集合住宅」を判定する
+// Google側のtypesに業種が含まれない住宅系の建物（マンション等）を拾うためのフォールバック
+const APARTMENT_NAME_KEYWORDS = [
+  "マンション",
+  "ハイツ",
+  "コーポ",
+  "アパート",
+  "タワー",
+  "レジデンス",
+  "ヴィラ",
+  "パレス",
+  "コート",
+  "ハイム",
+  "テラス",
+  "ハウス",
+  "ドミール",
+  "ヒルズ",
+  "スカイ",
+  "ガーデン",
+  "パーク",
+  "フラット",
+  "メゾン",
+];
+
+/**
+ * 建物名から「集合住宅」らしさを判定する
+ * @param {string} name
+ * @returns {boolean}
+ */
+function looksLikeApartment(name = "") {
+  return APARTMENT_NAME_KEYWORDS.some((keyword) => name.includes(keyword));
+}
 
 /**
  * Places APIのtypes配列から、日本語の業種ラベルを1つ選んで返す
+ * Google側の返却順ではなく、PRIORITY_ORDER（自前の優先順位）に沿って判定する
+ * 業種が判定できない場合、建物名から「集合住宅」かどうかをフォールバック判定する
  * @param {string[]} types
+ * @param {string} name
  * @returns {string}
  */
-function getJapaneseLabel(types = []) {
-  for (const t of types) {
-    if (TYPE_LABELS[t]) return TYPE_LABELS[t];
+function getJapaneseLabel(types = [], name = "") {
+  for (const priorityType of PRIORITY_ORDER) {
+    if (types.includes(priorityType)) {
+      return TYPE_LABELS[priorityType];
+    }
+  }
+  if (looksLikeApartment(name)) {
+    return "集合住宅";
   }
   return "施設"; // どれにもマッチしなかった場合のデフォルト
 }
 
-module.exports = { getJapaneseLabel, TYPE_LABELS };
+module.exports = { getJapaneseLabel, TYPE_LABELS, PRIORITY_ORDER };
